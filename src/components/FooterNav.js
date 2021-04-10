ddbcfe4497b77e04a9010e53079e9c3b14b9d5a1
Stackbit Code Editor <projects@stackbit.com>
2021-04-10 20:49:47 +0000

    FooterNav.js: changed

--- src/components/FooterNav.js
+++ src/components/FooterNav.js
@@ -8,7 +8,18 @@ export default class FooterNav extends React.Component {
         let section = _.get(this.props, 'section', null);
         return (
             <section className="cell widget widget-nav">
-<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3580.677137159693!2d84.38041601502943!3d26.17464238345103!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3992fbda74189493%3A0x38df1d153c23a1ab!2sExcellent%20Industrial%20Training%20Institute!5e0!3m2!1sen!2sin!4v1618087069778!5m2!1sen!2sin" width="400" height="300" style={{border:0}} allowfullscreen="" aria-hidden="false" tabindex="0"></iframe>
+              {_.get(section, 'title', null) && (
+              <h2 className="widget-title">{_.get(section, 'title', null)}</h2>
+              )}
+              {_.get(section, 'nav_links', null) && (
+              <ul className="menu">
+                {_.map(_.get(section, 'nav_links', null), (action, action_idx) => (
+                <li key={action_idx} className="menu-item">
+                  <Action {...this.props} action={action} />
+                </li>
+                ))}
+              </ul>
+              )}
             </section>
         );
     }
