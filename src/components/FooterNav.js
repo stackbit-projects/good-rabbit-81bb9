028bbf676f2fe66ff26a6dd12a88d3ac27b8a366
Stackbit Code Editor <projects@stackbit.com>
2021-04-10 20:51:12 +0000

    FooterNav.js: changed

--- src/components/FooterNav.js
+++ src/components/FooterNav.js
@@ -13,7 +13,7 @@ export default class FooterNav extends React.Component {
               )}
               {_.get(section, 'nav_links', null) && (
                     <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3580.677137159693!2d84.38041601502943!3d26.17464238345103!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3992fbda74189493%3A0x38df1d153c23a1ab!2sExcellent%20Industrial%20Training%20Institute!5e0!3m2!1sen!2sin!4v1618087069778!5m2!1sen!2sin" width="400" height="300" style={{border:0}} allowfullscreen="" aria-hidden="false" tabindex="0">
-            </iframe>
+                </iframe>
               )}
             </section>
         );
