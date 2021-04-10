b3e21296f3131e0dbb750d1e717e77730f1d1869
Stackbit Code Editor <projects@stackbit.com>
2021-04-10 20:42:09 +0000

    FooterForm.js: changed

--- src/components/FooterForm.js
+++ src/components/FooterForm.js
@@ -16,6 +16,7 @@ export default class FooterForm extends React.Component {
               <form name={_.get(section, 'form_id', null)} id={_.get(section, 'form_id', null)} {...(_.get(section, 'form_action', null) ? ({action: _.get(section, 'form_action', null)}) : null)} method="POST" data-netlify="true"
                 data-netlify-honeypot={_.get(section, 'form_id', null) + '-bot-field'}>
                 <div className="screen-reader-text">
+                    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3580.677137159693!2d84.38041601502943!3d26.17464238345103!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3992fbda74189493%3A0x38df1d153c23a1ab!2sExcellent%20Industrial%20Training%20Institute!5e0!3m2!1sen!2sin!4v1618087069778!5m2!1sen!2sin" width="400" height="300" style="border:0;" allowfullscreen="" loading="lazy"></iframe>
 
                 </div>
                 <input aria-labelledby={_.get(section, 'form_id', null) + '-honeypot-label'} type="hidden" name="form-name"
