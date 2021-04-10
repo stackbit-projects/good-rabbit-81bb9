b7b97b24d4b7d244f202778fcf186be68e7d75ae
Stackbit Code Editor <projects@stackbit.com>
2021-04-10 20:42:05 +0000

    FooterForm.js: changed

--- src/components/FooterForm.js
+++ src/components/FooterForm.js
@@ -16,10 +16,7 @@ export default class FooterForm extends React.Component {
               <form name={_.get(section, 'form_id', null)} id={_.get(section, 'form_id', null)} {...(_.get(section, 'form_action', null) ? ({action: _.get(section, 'form_action', null)}) : null)} method="POST" data-netlify="true"
                 data-netlify-honeypot={_.get(section, 'form_id', null) + '-bot-field'}>
                 <div className="screen-reader-text">
-                  <label id={_.get(section, 'form_id', null) + '-honeypot-label'} htmlFor={_.get(section, 'form_id', null) + '-honeypot'}>Don't fill this out if you're
-                    human:</label>
-                  <input aria-labelledby={_.get(section, 'form_id', null) + '-honeypot-label'} id={_.get(section, 'form_id', null) + '-honeypot'}
-                    name={_.get(section, 'form_id', null) + '-bot-field'} />
+
                 </div>
                 <input aria-labelledby={_.get(section, 'form_id', null) + '-honeypot-label'} type="hidden" name="form-name"
                   value={_.get(section, 'form_id', null)} />
